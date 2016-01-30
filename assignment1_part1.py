06119c2b5e5d9f7c0a82c8e2ea21fc7e7e464761
Alex Doler <aedoler@gmail.com>
2016-01-30 22:34:53 +0000

    update

--- assignment1_part1.py
+++ assignment1_part1.py
@@ -3,29 +3,37 @@
 """Part 1"""
 
 class CustomerDivideException(Exception):
-    """Error"""
-    def init(self, message):
-        self.message = 'There was an error'
+    """Raise Exception"""
+
 
 def listDivide(numbers, divide=2):
     """return amount of numbers in numbers list divisible by divide.
     """
     counter = 0
     for number in numbers:
-        if number % 2 == 0:
+        if number % divide == 0:
             counter += 1
     return counter
 
 
 def testListDivide():
+    """Docstring"""
     test1 = listDivide([1,2,3,4,5])
     test2 = listDivide([2,4,6,8,10])
-    test3 = listDivide([30, 54, 63,98, 100], 10)
+    test3 = listDivide([30, 54, 63,98, 100], divide=10)
     test4 = listDivide([])
     test5 = listDivide([1,2,3,4,5], 1)
     
-    return test1, test2, test3, test4, test5
-    
-print listDivide([30, 54, 63, 98, 100], 10)
+    tests = (test1, test2, test3, test4, test5)
+    while test1 == int(2):
+        while test2 == int(5):
+            while test3 == int(2):
+                while test4 == int(0):
+                    while test5 == int(5):
+                        return tests
+    else:
+        raise CustomerDivideException('Exception: a number is incorrect')
+
+
 
 print testListDivide()
